bb62b9b2b132ca629c382cb7c407f8ac9a047d7c
JuanDavid_Dev <juandavid.dev@gmail.com>
2024-09-10 20:05:17 -0500

    V.0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# starbucks-calculadora-propinas-ts
+ Web app con React TS para hacer pedido de productos y calculo de propinas
---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export function formatCurrency(quantity:number) {
+    return new Intl.NumberFormat('en-Us',{
+        style:'currency',currency:'USD'
+    }).format(quantity)
+}
